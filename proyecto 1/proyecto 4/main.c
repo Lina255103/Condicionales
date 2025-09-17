dfd0a1b6d35f87b348c6a3f01a1b5469c17a130d
Lina255103 <lfninon@udistrital.edu.co>
2025-09-17 09:57:26 -0500

    Add files via upload

---
+++ proyecto 1/proyecto 4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+	int age = 17;
+	
+	if(age>=18) {
+		printf("You are eligible to vote.\n");
+	} else {
+		printf("You are not yet eligible to vote.n");
+	}
+	return 0;
+}
